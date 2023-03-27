50111357ac5d4b4d9afe38f0809acced7d39e8f4
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-03-27 16:33:43 +1100

    Initial commit.

---
+++ NeuroForge.TestClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuroForge.TestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
